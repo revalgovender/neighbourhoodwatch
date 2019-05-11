4adb9840e3966dcc4ca3457355ba93fad34f2813
revalgovender <reval23@yahoo.com>
2019-05-11 16:30:34 +0100

    add sns topic + publish to SNS topic when new report comes in

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -9,7 +9,7 @@ import {FormControl} from '@angular/forms';
 })
 export class AppComponent {
   title = 'neighbourhood-watch';
-  awsSnsEndpoint = 'https://6gz8r9jr2j.execute-api.eu-west-1.amazonaws.com/dev/report/create';
+  awsSnsEndpoint = 'https://smon7aby8a.execute-api.eu-west-1.amazonaws.com/dev/report/create';
   reportMessage = new FormControl('');
   httpOptions = {
     headers: new HttpHeaders({
--- handler.py
+++ handler.py
@@ -1,11 +1,34 @@
 import json
 import boto3
+import logging
+
+def publish_to_sns(message):
+    sns = boto3.client('sns')
+    return sns.publish(
+        TopicArn='arn:aws:sns:eu-west-1:769403605349:SendReportTopic',
+        Message=message,
+        MessageStructure='string',
+        MessageAttributes={
+            'summary': {
+                'StringValue': 'just a summary',
+                'DataType': 'String'
+            }
+        }
+    )
 
 
 def add_report(event, context):
+    # decide when the function should begin to send SNS message and leave
+    threshold_millis = 10 * 1000  # leave when there are only 10 seconds left
+
     report = json.loads(event['body'])
     reports_table = boto3.resource('dynamodb').Table('reports')
     response = reports_table.put_item(Item=report)
+
+    # check remain time by accessing context object
+    remain_millis = context.get_remaining_time_in_millis()
+    if remain_millis < threshold_millis:
+        publish_to_sns(report['message'])
 
     body = {
         "response": response
@@ -13,7 +36,7 @@ def add_report(event, context):
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(event)
+        "body": json.dumps(response)
     }
 
     return response
