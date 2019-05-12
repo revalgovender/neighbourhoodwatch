425f7bcaa192c5fe9766fffb0b0cf177e4197aad
revalgovender <reval23@yahoo.com>
2019-05-12 09:22:18 +0100

    add form validation + add success screen

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,6 +10,9 @@ import {FormControl} from '@angular/forms';
 export class AppComponent {
   title = 'neighbourhood-watch';
   awsSnsEndpoint = 'https://smon7aby8a.execute-api.eu-west-1.amazonaws.com/dev/report/create';
+  showReportMessageValidationErrorMessage = false;
+  showReportForm = true;
+  showSuccessMessage = false;
   reportMessage = new FormControl('');
   httpOptions = {
     headers: new HttpHeaders({
@@ -25,13 +28,31 @@ export class AppComponent {
 
   sendToSns() {
     let body = {
-      'message': this.reportMessage.value
+      'message': this.reportMessage.value + ' - Please do not respond to this text.'
     };
 
     this.http.post(this.awsSnsEndpoint, body, this.httpOptions).subscribe();
   }
 
   submitReport() {
+    if (!this.formIsValid()) {
+      this.showReportMessageValidationErrorMessage = true;
+      return false;
+    }
+
+    this.showReportMessageValidationErrorMessage = false;
+    this.showReportForm = false;
+    this.showSuccessMessage = true;
     this.sendToSns();
+
+    return true;
+  }
+
+  private formIsValid() {
+    if (this.reportMessage.value) {
+      return true;
+    }
+
+    return false;
   }
 }
